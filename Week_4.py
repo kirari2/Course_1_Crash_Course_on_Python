6a814e2c8c053ca48e6d2afe7511608d71ce4310
kirari2 <113248026+kirari2@users.noreply.github.com>
2023-04-15 22:25:41 +0800

    Update Week_4.py

--- Week_4.py
+++ Week_4.py
@@ -35,8 +35,7 @@ def groups_per_user(group_dictionary):
         return (user_groups)
 
 
-print(groups_per_user({"local": ["admin", "userA"], "public": ["admin", "userB"], "administrator": ["admin"]}))
-# Should print: {'admin': ['local', 'public', 'administrator'], 'userA': ['local'], 'userB': ['public']}
+print(groups_per_user({"local": ["admin", "userA"], "public": ["admin", "userB"], "administrator": ["admin"]}))  # Should print: {'admin': ['local', 'public', 'administrator'], 'userA': ['local'], 'userB': ['public']}
 
 # Question 3
 # The dict.update method updates one dictionary with the items coming from the other dictionary, so that existing entries are replaced and new entries are added. 
@@ -45,5 +44,29 @@ wardrobe = {'shirt': ['red', 'blue', 'white'], 'jeans': ['blue', 'black']}
 new_items = {'jeans': ['white'], 'scarf': ['yellow'], 'socks': ['black', 'brown']}
 wardrobe.update(new_items)
 
-print(wardrobe.items())
-# Should print: dict_items([('shirt', ['red', 'blue', 'white']), ('jeans', ['white']), ('scarf', ['yellow']), ('socks', ['black', 'brown'])])
+print(wardrobe.items())  # Should print: dict_items([('shirt', ['red', 'blue', 'white']), ('jeans', ['white']), ('scarf', ['yellow']), ('socks', ['black', 'brown'])])
+
+# Question 4
+# What’s a major advantage of using dictionaries over lists?
+# Answer: It’s quicker and easier to find a specific element in a dictionary
+
+# Question 5
+# The add_prices function returns the total price of all of the groceries in the  dictionary. Fill in the blanks to complete this function.
+
+def add_prices(basket):
+    # Initialize the variable that will be used for the calculation
+    total = 0
+    # Iterate through the dictionary items
+    for key, value in basket.items():
+        # Add each price to the total calculation
+        # Hint: how do you access the values of
+        # dictionary items?
+        total += basket[key]
+    # Limit the return value to 2 decimal places
+    return round(total, 2)
+
+
+groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99, "bread": 4.59,
+             "coffee": 6.99, "milk": 3.39, "eggs": 2.98, "cheese": 5.44}
+
+print(add_prices(groceries))  # Should print 28.44
