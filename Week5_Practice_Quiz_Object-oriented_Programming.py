6dc9cc8b0702e460a74946bc3c812280d1ff3cbd
kirari2 <113248026+kirari2@users.noreply.github.com>
2023-04-23 10:06:22 +0800

    Update Week5_Practice_Quiz_Object-oriented_Programming.py

--- Week5_Practice_Quiz_Object-oriented_Programming.py
+++ Week5_Practice_Quiz_Object-oriented_Programming.py
@@ -30,13 +30,13 @@ martin.apples = 2
 martin.ideas = 1
 
 def exchange_apples(you, me):
-#Here, despite G.B. Shaw's quote, our characters have started with       
-#different amounts of apples so we can better observe the results. 
-#We're going to have Martin and Johanna exchange ALL their apples with #one another.
-#Hint: how would you switch values of variables, 
-#so that "you" and "me" will exchange ALL their apples with one another?
-#Do you need a temporary variable to store one of the values?
-#You may need more than one line of code to do that, which is OK. 
+# Here, despite G.B. Shaw's quote, our characters have started with       
+# different amounts of apples so we can better observe the results. 
+# We're going to have Martin and Johanna exchange ALL their apples with #one another.
+# Hint: how would you switch values of variables, 
+# so that "you" and "me" will exchange ALL their apples with one another?
+# Do you need a temporary variable to store one of the values?
+# You may need more than one line of code to do that, which is OK. 
         temp = 0
         temp = you.apples
         you.apples = me.apples
@@ -61,5 +61,69 @@ print("Johanna has {} apples and Martin has {} apples".format(johanna.apples, ma
 exchange_ideas(johanna, martin)
 print("Johanna has {} ideas and Martin has {} ideas".format(johanna.ideas, martin.ideas))
 
+# Question 3
+# The City class has the following attributes: name, country (where the city is located), elevation (measured in meters), and population (approximate, according to recent statistics). Fill in the blanks of the max_elevation_city function to return the name of the city and its country (separated by a comma), when comparing the 3 defined instances for a specified minimal population. For example, calling the function for a minimum population of 1 million: max_elevation_city(1000000) should return "Sofia, Bulgaria". 
 
+# define a basic city class
+class City:
+	name = ""
+	country = ""
+	elevation = 0 
+	population = 0
+
+# create a new instance of the City class and
+# define each attribute
+city1 = City()
+city1.name = "Cusco"
+city1.country = "Peru"
+city1.elevation = 3399
+city1.population = 358052
+
+# create a new instance of the City class and
+# define each attribute
+city2 = City()
+city2.name = "Sofia"
+city2.country = "Bulgaria"
+city2.elevation = 2290
+city2.population = 1241675
+
+# create a new instance of the City class and
+# define each attribute
+city3 = City()
+city3.name = "Seoul"
+city3.country = "South Korea"
+city3.elevation = 38
+city3.population = 9733509
+
+def max_elevation_city(min_population):
+# Initialize the variable that will hold 
+# the information of the city with 
+# the highest elevation 
+	return_city = City()
+
+	# Evaluate the 1st instance to meet the requirements:
+	# does city #1 have at least min_population and
+	# is its elevation the highest evaluated so far?
+	if city1.population < min_population:
+		return_city = city1
+	# Evaluate the 2nd instance to meet the requirements:
+	# does city #2 have at least min_population and
+	# is its elevation the highest evaluated so far?
+	if city2.population < min_population and city2.population > city1.population:
+		return_city = city2
+	# Evaluate the 3rd instance to meet the requirements:
+	# does city #3 have at least min_population and
+	# is its elevation the highest evaluated so far?
+	if city3.population < city2.population and city3.population > city2.population:
+		return_city = city3
+
+	#Format the return string
+	if return_city.name:
+		return str(return_city.name + ", " + return_city.country)
+	else:
+		return ""
+
+print(max_elevation_city(100000)) # Should print "Cusco, Peru"
+print(max_elevation_city(1000000)) # Should print "Sofia, Bulgaria"
+print(max_elevation_city(10000000)) # Should print ""
 
